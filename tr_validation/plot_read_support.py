88196869f913cd37d976ffd0915da5f4d899651a
Tom Sasani <tomsasani1493@gmail.com>
2024-01-23 08:22:11 -0700

    jit bootstrap in read support plotting method

--- tr_validation/plot_read_support.py
+++ tr_validation/plot_read_support.py
@@ -2,141 +2,197 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import argparse
-import itertools
 import numpy as np
+from typing import Callable
+import numba
+
+plt.rc("font", size=10)
+
+
+@numba.njit
+def compute_nanmean(a: np.ndarray, row: bool = True) -> np.ndarray:
+    """Compute the sum of a 2D numpy array
+    on a per-row basis, ignoring nans. Since `numba` does not
+    support kwargs in the `np.nansum` function, it's
+    necessary to piece this out into its own function
+    so that it can be decorated with `numba.njit`.
+
+    Args:
+        a (np.ndarray): A 2D numpy array of size (N, M).
+
+        row (bool, optional): Whether to calculate means by row or column. Defaults to True.
+
+
+    Returns:
+        rowsums (np.ndarray): A 1D numpy array of size (N, ) containing \
+            sums of every row in the input.
+    """
+    idx = 0 if row else 1
+    empty_a = np.zeros(a.shape[idx])
+    for i in np.arange(a.shape[idx]):
+        empty_a[i] = np.nanmean(a[i]) if row else np.nanmean(a[:, i])
+    return empty_a
+
+
+@numba.njit(parallel=True)
+def bootstrap(X: np.ndarray, n: int = 100):
+    means = np.zeros((n, X.shape[1]))
+
+    i = X.shape[0]
+    for boot_i in numba.prange(n):
+        idxs = np.random.randint(0, i, size=i)
+        resampled = X[idxs, :]
+        mean = compute_nanmean(resampled, row=False)
+        means[boot_i, :] = mean
+    return means
+
+
+def classify_motif(motif: str):
+    motif_len = len(motif)
+    if motif_len < 5:
+        return str(motif_len)
+    else:
+        return "5+"
+
+
+def calculate_min_dist(row: pd.Series):
+    # figure out whether the observed diff is closer
+    # to the expected ALT or REF allele size
+    alt_distance = row["diff"] - row["exp_allele_diff_alt"]
+    ref_distance = row["diff"] - row["exp_allele_diff_ref"]
+
+    # if the diff is closer to the REF allele length, we'll assume
+    # that it is a REF observation. and vice versa
+    if abs(ref_distance) < abs(alt_distance):
+        is_ref = True
+    elif abs(ref_distance) > abs(alt_distance):
+        is_ref = False
+    # if they're equal, we'll randomly assign the diff to be a REF/ALT obs
+    else:
+        is_ref = np.random.uniform() > 0.5
+
+    return "REF" if is_ref else "ALT"
 
 
 def main(args):
-    res_df = pd.read_csv(args.csv)
-    print(res_df.drop_duplicates("region").shape)
+    numba.set_num_threads(args.threads)
 
-    res_df_totals = (
-        res_df.query("sample == 'kid'")
-        .groupby("region")
+    res_df = pd.read_csv(args.csv)
+
+    # add a homopolymer column if it's not in there
+    if "Is homopolymer?" not in res_df:
+        res_df["Is homopolymer?"] = res_df["motif"].apply(lambda m: len(m) == 1)
+
+    # for each read, calculate the minimum distance to either the REF or ALT allele
+    # (i.e., the "closest" allele)
+    res_df["is_ref"] = res_df.apply(lambda row: calculate_min_dist(row), axis=1)
+    res_df["Expected allele length"] = res_df.apply(
+        lambda row: row["exp_allele_diff_ref"]
+        if row["is_ref"] == "REF"
+        else row["exp_allele_diff_alt"],
+        axis=1,
+    )
+    res_df["measured_allele_length_error"] = (
+        res_df["diff"] - res_df["Expected allele length"]
+    )
+
+    # subset to kid
+    kid_df = res_df[res_df["sample"] == "kid"]
+    kid_df_totals = (
+        kid_df.groupby("region")
         .agg({"Read support": "sum"})
         .reset_index()
         .rename(columns={"Read support": "Total read support"})
     )
-    res_df = res_df.merge(res_df_totals, on="region", how="left")
-
-    print(res_df.drop_duplicates("region").shape)
-
+    kid_df = kid_df.merge(kid_df_totals, on="region", how="left")
     # require at least 10 total spanning reads
-    res_df = res_df[res_df["Total read support"] >= 10]
-    print(res_df.drop_duplicates("region").shape)
-    # require at least 2 reads for a given observation of allele length
-    res_df["Fraction of reads"] = res_df["Read support"] / res_df["Total read support"]
+    kid_df = kid_df[kid_df["Total read support"] >= 10]
 
-    # require an observation to be supported by at least 5% of reads
-    # res_df = res_df[res_df["frac"] >= 0.05]
+    col = "measured_allele_length_error"
 
-    # add a homopolymer column if it's not in there
-    if "is_homopolymer" not in res_df:
-        res_df["is_homopolymer"] = res_df["motif"].apply(lambda m: len(m) == 1)
+    f, axarr = plt.subplots(2, 2, sharex=True, sharey=False, figsize=(12, 6))
 
-    # ignore sites where the ref and alt ALs are identical
-    res_df = res_df[res_df["exp_allele_diff_ref"] != res_df["exp_allele_diff_alt"]]
+    group2ax = {
+        ("REF", True): (0, 0),
+        ("REF", False): (0, 1),
+        ("ALT", True): (1, 0),
+        ("ALT", False): (1, 1),
+    }
 
-    res_df["Matches REF"] = res_df["diff"] == res_df["exp_allele_diff_ref"]
-    res_df["Matches ALT"] = res_df["diff"] == res_df["exp_allele_diff_alt"]
+    for sub, kid_df_sub in kid_df.groupby(["is_ref", "Is homopolymer?"]):
+        is_ref, is_homo = sub
+        ax_i, ax_j = group2ax[(is_ref, is_homo)]
 
-    res_df_parental_ev = (
-        res_df[res_df["sample"] != "kid"]
-        .groupby(["region", "sample", "Matches ALT"])
-        .size()
-        .reset_index()
-        .rename(columns={0: "count"})
-    )
-    res_df_parental_ev = res_df_parental_ev[res_df_parental_ev["Matches ALT"] == True]
+        # figure out min and max of error
+        emin, emax = kid_df_sub[col].min(), kid_df_sub[col].max()
+        # and number of loci
+        n_loci = kid_df_sub["region"].nunique()
+        loc2idx = dict(zip(kid_df_sub["region"].unique(), range(n_loci)))
+        kid_df_sub["region_idx"] = kid_df_sub["region"].apply(lambda r: loc2idx[r])
 
-    to_filter = res_df_parental_ev["region"].to_list()
+        arr = np.zeros((n_loci, emax - emin), dtype=np.int16)
 
-    # remove sites where we see evidence in parents
-    res_df["Evidence for DNM allele in parents?"] = res_df["region"].apply(
-        lambda r: r in to_filter
-    )
-    res_df = res_df[res_df["Evidence for DNM allele in parents?"] == False]
+        # increment the array
+        region_idxs = kid_df_sub["region_idx"].values
+        measured_al_idxs = kid_df_sub[col].values - emin - 1
+        vals = kid_df_sub["Read support"].values
 
-    res_df["diff_to_ref"] = res_df["diff"] - res_df["exp_allele_diff_ref"]
+        arr[region_idxs, measured_al_idxs] = vals
 
-    if "n_with_denovo_allele_strict" in res_df.columns:
-        res_df["Is transmitted?"] = res_df["n_with_denovo_allele_strict"] > 0
-    else:
-        res_df["Is transmitted?"] = "unknown"
+        arr_sums = np.nansum(arr, axis=1)
+        arr_fracs = arr / arr_sums.reshape(-1, 1)
 
-    # plot the fraction of reads supporting each call
-    print (res_df[(res_df["Matches ALT"] == True) & (res_df["Fraction of reads"] < 0.1)][["region", "diff", "exp_allele_diff_alt", "Fraction of reads"]])
+        bs_fracs = bootstrap(arr_fracs)
+        bs_fracs_l = np.nanpercentile(bs_fracs, q=2.5, axis=0)
+        bs_fracs_u = np.nanpercentile(bs_fracs, q=97.5, axis=0)
 
-    g = sns.FacetGrid(
-        data=res_df[res_df["Matches ALT"] == True],
-        #row="is_homopolymer",
-        col="Is transmitted?",
-    )
-    # g.map(sns.boxplot,
-    #     #data=res_df[res_df["Matches ALT"] == True],
-    #     "is_homopolymer",
-    #     "Fraction of reads",
-    #             "is_homopolymer",
+        x = np.arange(emin, emax) + 1
 
-    #     #palette="white",
-    #     fliersize=0,
-    # )
-    g.map(sns.stripplot,
-        #data=res_df[res_df["Matches ALT"] == True],
-        "is_homopolymer",
-        "Fraction of reads",
-                "is_homopolymer",
+        axarr[ax_i, ax_j].plot(x, np.nanmean(arr_fracs, axis=0), c="grey")
+        axarr[ax_i, ax_j].fill_between(
+            x, bs_fracs_l, bs_fracs_u, alpha=0.75, color="gainsboro"
+        )
 
-    )
-    # ax.set_ylim(0, 1.05)
-    # ax.set_xlabel("Is homopolymer?")
-    # ax.set_ylabel("Fraction of reads supporting ALT allele")
-    # ax.legend([], [], frameon=False)
-    pref = args.out.split("/")[-1].split(".")[0]
-    # sns.despine(ax=ax, top=True, right=True)
-    # f.tight_layout()
-    g.savefig(f"tr_validation/fig/{pref}.support.png", dpi=200)
-
-    res_df = res_df.query("diff_to_ref != 0")
-
-    if "n_with_denovo_allele_strict" in res_df.columns:
-        res_df["Is transmitted?"] = res_df["n_with_denovo_allele_strict"] > 0
-    else:
-        res_df["Is transmitted?"] = "unknown"
-
-    x, y = "exp_allele_diff_alt", "diff"
-
-    g = sns.relplot(
-        data=res_df[(res_df["sample"] == "kid") & (res_df["Matches REF"] == False)],
-        x=x,
-        y=y,
-        col="is_homopolymer",
-        row="Evidence for DNM allele in parents?",
-        hue="Fraction of reads",
-        style="Is transmitted?",
-        s=100,
-    )
-    g.map(plt.axline, xy1=(0, 0), slope=1, ls=":", c="k", lw=2, zorder=-1)
-
-    # titles = [
-    #     r"$\bf{a)}$" + " candidate STR DNMs (not in homopolymers)",
-    #     r"$\bf{b)}$" + " candidate STR DNMs in homopolymers",
-    # ]
-    # for i, ax in enumerate(g.axes.flat):
-    #     ax.set_title(titles[i], fontsize=10)
-    #     ax.set_xlabel(
-    #         "Measured allele length difference\n(read vs. REF, using Element CIGAR)",
-    #     )
-    #     ax.set_ylabel(
-    #         "Expected allele length difference\n(ALT vs. REF, using TRGT)",
-    #     )
-    g.tight_layout()
-    g.savefig(args.out, dpi=200)
+        axarr[ax_i, ax_j].set_xlabel("Difference between Element and TRGT AL (bp)")
+        axarr[ax_i, ax_j].set_ylabel("Fraction of reads at a locus")
+        homopol_status = "homopolymers" if is_homo else "non-homopolymers"
+        axarr[ax_i, ax_j].set_title(
+            f"Reads supporting the\n{is_ref} allele at {homopol_status}"
+        )
+        sns.despine(ax=axarr[ax_i, ax_j], top=True, right=True)
+    f.tight_layout()
+    f.savefig(args.out, dpi=200)
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--csv")
-    p.add_argument("--out")
+    p.add_argument(
+        "--csv",
+        type=str,
+        help="""Path to CSV file containing estimated allele lengths.""",
+    )
+    p.add_argument(
+        "--out",
+        type=str,
+        help="""Path to output image.""",
+    )
+    p.add_argument(
+        "-tech",
+        type=str,
+        default="illumina",
+        help="""Technology used to generate allele length estimates.""",
+    )
+    p.add_argument(
+        "-pctile",
+        type=int,
+        default=95,
+        help="""Percentile to plot surrounding mean estimates using a bootstrap.""",
+    )
+    p.add_argument(
+        "-threads",
+        type=int,
+        default=1,
+        help="""Number of threads to use for bootstrap estimation.""",
+    )
     args = p.parse_args()
     main(args)
