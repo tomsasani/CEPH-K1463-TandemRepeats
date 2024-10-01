e250de1dcb1aed958573ff7bd5ebf8821509afbe
Tom Sasani <tomsasani1493@gmail.com>
2024-10-01 07:27:22 -0600

    compute reference AL in denominator calculations

--- tr_validation/calculate_grouped_denominator.py
+++ tr_validation/calculate_grouped_denominator.py
@@ -3,6 +3,7 @@ import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
 import utils
+import numpy as np
 
 plt.rc("font", size=14)
 
@@ -58,6 +59,15 @@ def main(args):
     raw_loci["motif_size"] = raw_loci.apply(lambda row: utils.determine_motif_size(row), axis=1)
     raw_loci["simple_motif_size"] = raw_loci.apply(lambda row: utils.determine_simplified_motif_size(row), axis=1)
 
+    # add column with binned reference length
+    raw_loci["reflen"] = raw_loci["end"] - raw_loci["start"]
+    min_reflen, max_reflen = raw_loci["reflen"].min(), raw_loci["reflen"].max()
+
+    reflen_bins = np.arange(min_reflen, max_reflen, 10)
+    bins, labels = pd.cut(raw_loci["reflen"].values, bins=reflen_bins, retbins=True)
+
+    raw_loci["reflen_bin"] = bins
+
     # calculate total number of base pairs
     raw_loci["aff_bp"] = raw_loci["end"] - raw_loci["start"]
     total_bp = raw_loci["aff_bp"].sum()
@@ -73,11 +83,11 @@ def main(args):
     insufficient_depth_sites = list(set(insufficient_depth_sites))
     raw_loci = raw_loci[~raw_loci["trid"].isin(insufficient_depth_sites)]
 
-    GROUP_COLS = ["motif_size", "simple_motif_size", "#chrom"]
+    GROUP_COLS = ["motif_size", "simple_motif_size", "reflen_bin", "#chrom"]
 
     res = []
     # loop over samples grouped by motif size to start
-    for (motif_size, simple_motif_size, chrom), sub_df in tqdm.tqdm(raw_loci.groupby(GROUP_COLS)):
+    for (motif_size, simple_motif_size, reflen_bin, chrom), sub_df in tqdm.tqdm(raw_loci.groupby(GROUP_COLS)):
         # denominator is the number of annotated loci of the specified motif size
         # in the specified sample
         denom = sub_df.shape[0]
@@ -86,6 +96,7 @@ def main(args):
             {
                 "motif_size": motif_size,
                 "simple_motif_size": simple_motif_size,
+                "reflen_bin": reflen_bin,
                 "chrom": chrom,
                 "denominator": denom,
             }
