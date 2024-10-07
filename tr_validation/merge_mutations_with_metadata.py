c38bc153d01249b714473ecf55c7986b37464518
Tom Sasani <tomsasani1493@gmail.com>
2024-10-07 13:04:53 -0600

    use denovo size filtering after merging

--- tr_validation/merge_mutations_with_metadata.py
+++ tr_validation/merge_mutations_with_metadata.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import argparse
 import numpy as np
+import glob
 
 import utils
+
 
 def main(args):
     raw_denovos = pd.read_csv(
@@ -24,33 +26,16 @@ def main(args):
         dtype={"sample_id": str},
     )
 
-    multiples = raw_denovos.groupby(["trid", "genotype"]).size().to_dict()
-    multiple_trids = [k[0] for k,v in multiples.items() if v > 1]
-    for c in raw_denovos.columns:
-        raw_vals, trans_vals = raw_denovos[c].values, transmission_evidence[c].values
-        if not np.array_equal(raw_vals, trans_vals):
-            for i in range(raw_vals.shape[0]):
-                if raw_vals[i] != trans_vals[i]:
-                    print (raw_vals[i], trans_vals[i])
-
     raw_plus_transmission = raw_denovos.merge(transmission_evidence)
 
-    # for (trid, genotype), sub_df in raw_plus_transmission.groupby(["trid", "genotype"]):
-    #     if sub_df.shape[0] > 1:
-    #         for c in sub_df.columns:
-    #             if np.unique(sub_df[c].values).shape[0] > 1:
-    #                 print (c)
+    mutations = raw_plus_transmission.merge(phasing, how="left").fillna({"phase_summary": "unknown"})
+    mutations["likely_denovo_size"] = mutations.apply(lambda row: utils.add_likely_de_novo_size(row, use_phase=True), axis=1)
 
-    final = raw_plus_transmission.merge(phasing, how="left").fillna({"phase_summary": "unknown"})
-
-    final["likely_denovo_size"] = final.apply(lambda row: utils.add_likely_de_novo_size(row, use_phase=True), axis=1)
-
-    # ensure we're only looking at sites where the de novo size at least as big as the motif size.
-    # if a complex locus contains a homopolymer, allow for 1bp expansions/contractions
-    final = final[
-        (final["motif_size"].isin([-1, 1])) | (np.abs(final["likely_denovo_size"]) >= 2)
+    # if we 're not at a homopolymer, ensure that the likely de novo size is at least 2 bp
+    mutations = mutations[
+        (mutations["motif_size"].isin([-1, 1])) | (np.abs(mutations["likely_denovo_size"]) >= 2)
     ]
-    final.to_csv(args.out, index=False, sep="\t")
+    mutations.to_csv(args.out, index=False, sep="\t")
 
 
 if __name__ == "__main__":
@@ -59,5 +44,6 @@ if __name__ == "__main__":
     p.add_argument("--transmission_evidence")
     p.add_argument("--phasing")
     p.add_argument("--out")
+    p.add_argument("--assembly")
     args = p.parse_args()
     main(args)
