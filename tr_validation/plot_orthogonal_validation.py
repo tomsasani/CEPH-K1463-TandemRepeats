7c319af2e71405d0917d2ca360f00f636ba3567f
Tom Sasani <tomsasani1493@gmail.com>
2024-10-08 14:43:49 -0600

    include POI annotation in orthogonal validation plots

--- tr_validation/plot_orthogonal_validation.py
+++ tr_validation/plot_orthogonal_validation.py
@@ -6,28 +6,26 @@ import glob
 
 
 ASSEMBLY = "CHM13v2"
-# ASSEMBLY = "GRCh38"
-TECH = "hifi"
-MIN_SIZE = 1
+ORTHOGONAL_TECH = "hifi"
+MIN_SIZE = 50
+MIN_MOTIF_SIZE = 1
 
-# get mutations
+# read in all per-sample DNM files
 mutations = []
-for fh in glob.glob(f"tr_validation/csv/orthogonal_support/*.{ASSEMBLY}.{TECH}.tsv"):
-    df = pd.read_csv(
-        fh,
-        sep="\t",
-        dtype={"sample_id": str},
-    )
+for fh in glob.glob(f"tr_validation/csv/orthogonal_support/*.{ASSEMBLY}.{ORTHOGONAL_TECH}.tsv"):
+    df = pd.read_csv(fh, sep="\t", dtype={"sample_id": str})
     mutations.append(df)
+mutations = pd.concat(mutations).fillna({"children_with_denovo_allele": "unknown", "phase_summary": "unknown"})
 
-
-mutations = pd.concat(mutations)
-# mutations = mutations[~mutations["sample_id"].str.startswith("200")]
+# ensure we're looking at G3 DNMs
 mutations = mutations[mutations["paternal_id"] == 2209]
 
 # FILTERING
 mutations = mutations[np.abs(mutations["likely_denovo_size"]) >= MIN_SIZE]
-mutations = mutations[mutations["motif_size"] >= 6]
+mutations = mutations[mutations["motif_size"] >= MIN_MOTIF_SIZE]
+
+mutations = mutations[mutations["validation_status"] != "no_data"]
+
 mutations["reference_al"] = mutations["end"] - mutations["start"]
 
 # calculate child-to-reference diff
@@ -36,10 +34,6 @@ mutations["child_to_reference_diff"] = (
 )
 mutations["child_to_parent_diff"] = mutations["likely_denovo_size"]
 
-mutations["is_homopolymer"] = mutations["max_motiflen"] == 1
-
-mutations = mutations[mutations["validation_status"] != "no_data"]
-
 
 mutations = mutations.sort_values(["alt_sample_id", "trid"])
 
@@ -47,10 +41,10 @@ for (sample, trid, genotype), trid_df in mutations.groupby(
     ["alt_sample_id", "trid", "genotype"]
 ):
     validation = trid_df["validation_status"].unique()[0]
-    motif_size = trid_df["max_motiflen"].values[0]
+    motif_size = trid_df["motif_size"].values[0]
     poi = trid_df["phase_summary"].unique()[0]
     size = trid_df["likely_denovo_size"].unique()[0]
-    is_homopolymer = trid_df["is_homopolymer"].unique()[0]
+
 
     if poi != "unknown":
         poi = poi.split(":")[0] if int(poi.split(":")[1]) > 5 else "unknown"
@@ -126,15 +120,16 @@ for (sample, trid, genotype), trid_df in mutations.groupby(
             c="firebrick",
             label="Expected allele length (non de novo)",
         )
-    ax.set_ylabel(f"Allele length inferred using {TECH} reads")
+    ax.set_ylabel(f"Allele length inferred using {ORTHOGONAL_TECH} reads")
     ax.set_xlabel("Sample")
     ax.set_xticks(range(3))
     ax.set_xticklabels(["Kid", "Mom", "Dad"])
-    ax.set_title(f"{trid}\nMotif size = {motif_size}\n{size}")
+    ax.set_title(f"{trid}\nMotif size = {motif_size}\nDNM size = {size}")
     # ax.legend()
     sns.despine(ax=ax)
+    f.tight_layout()
     f.savefig(
-        f"tr_validation/fig/denovo/{TECH}/{ASSEMBLY}/{sample}.{trid}.png",
+        f"tr_validation/fig/denovo/{ORTHOGONAL_TECH}/{ASSEMBLY}/{sample}.{trid}.png",
         dpi=200,
     )
     plt.close()
