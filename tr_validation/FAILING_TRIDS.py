3bb7848a16e02264f89f2b5b8f14eb6b71f5116f
Tom Sasani <tomsasani1493@gmail.com>
2024-10-16 14:27:14 -0600

    remove 6bp motifs from failing VNTRs

--- tr_validation/FAILING_TRIDS.py
+++ tr_validation/FAILING_TRIDS.py
@@ -3,7 +3,10 @@ tr_validation/fig/denovo/hifi/CHM13v2/NA12882.chr10_134270808_134273029_trsolve.
 tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr2_239360734_239362665_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr13_13805712_13807827_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chrX_1178539_1179172_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12886.chr1_54316542_54317145_trsolve.png""".split()
+tr_validation/fig/denovo/hifi/CHM13v2/NA12886.chr1_54316542_54317145_trsolve.png
+/.chr11_87897_97405_trsolve
+/.chr22_18999263_18999516_TRF
+""".split()
 
 FAIL_SVS = [t.split("/")[-1].split(".")[1] for t in FAIL_SV_PATHS]
 
@@ -18,18 +21,13 @@ tr_validation/fig/denovo/hifi/CHM13v2/NA12882.chr10_134270808_134273029_trsolve.
 tr_validation/fig/denovo/hifi/CHM13v2/NA12882.chr12_123910297_123913954_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12882.chr15_99298545_99300911_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12883.chr5_550370_552106_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12883.chr7_156587500_156589426_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12883.chr18_49776737_49777085_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12883.chr20_65914234_65916562_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr1_246245491_246250266_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr2_241119715_241122763_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr7_156587500_156589426_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr16_94802470_94803543_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12884.chr20_64674757_64675640_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr2_241119715_241122763_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr2_242399998_242404718_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr3_148027446_148027537_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr7_156587500_156589426_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr11_446056_448419_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr13_113249907_113251906_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chr16_94802470_94803543_trsolve.png
@@ -38,14 +36,9 @@ tr_validation/fig/denovo/hifi/CHM13v2/NA12885.chrX_883371_890423_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12886.chr5_758538_762085_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12886.chr5_179136560_179137761_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12886.chr21_42096694_42097081_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr1_11586799_11586851_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr2_241119715_241122763_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr6_159468731_159468745_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr12_50561817_50562141_trsolve.png
-tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr20_64674757_64675640_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chr20_65348440_65348812_trsolve.png
 tr_validation/fig/denovo/hifi/CHM13v2/NA12887.chrX_883371_890423_trsolve.png
-/.chr11_87897_97405_trsolve
-/.chr22_18999263_18999516_TRF""".split()
+""".split()
 
 FAIL_VNTRS = [t.split("/")[-1].split(".")[1] for t in FAIL_VNTR_PATHS]
